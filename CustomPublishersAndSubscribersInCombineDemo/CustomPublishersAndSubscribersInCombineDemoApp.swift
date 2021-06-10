d5c74ef7aee66de17c606278e91f62ec84278eb3
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-10 15:40:47 -0600

    Initial Commit

---
+++ CustomPublishersAndSubscribersInCombineDemo/CustomPublishersAndSubscribersInCombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPublishersAndSubscribersInCombineDemoApp.swift
+//  CustomPublishersAndSubscribersInCombineDemo
+//
+//  Created by Fred Javalera on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomPublishersAndSubscribersInCombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
